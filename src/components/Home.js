a841da289ad4859881f071034415ea5b50ceb253
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-09-14 00:38:15 +0300

    README file edited

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home () {
     return (
-        <h1 id="welcome">iCare is a website intended to spread knowledge gained through life experience, fun facts, or whatever you would like! As long as it's kind, it is welcome.</h1>
+        <h1 id="welcome">iCare is a website where you may share knowledge you've learned from experience, interesting facts, or anything else you enjoy! Everything is welcome, provided it is polite.</h1>
     )
 }
 
